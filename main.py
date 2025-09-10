326037f470b14f551e010db6ac7df932d3d8e921
Thomas Dewitte <thomasdewittecontact@gmail.com>
2025-09-10 06:44:00 +0200

    Processor doesn't support CUDA so fixed it another way

--- main.py
+++ main.py
@@ -8,12 +8,18 @@ model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 model.to("cuda" if torch.cuda.is_available() else "cpu")
-processor.to("cuda" if torch.cuda.is_available() else "cpu")
 
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
-inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, return_tensors="pt", padding=True)
+inputs = processor(
+    text=["a photo of a cat", "a photo of a dog"],
+    images=image,
+    return_tensors="pt",
+    padding=True
+)
+
+inputs = {k: v.to(device) for k, v in inputs.items()}
 
 outputs = model(**inputs)
 logits_per_image = outputs.logits_per_image
