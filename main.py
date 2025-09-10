c80d457ba78700069b411f02356a11abb6b666ec
Thomas Dewitte <thomasdewittecontact@gmail.com>
2025-09-10 06:41:26 +0200

    Forgot also add processor to CUDA

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
 model.to("cuda" if torch.cuda.is_available() else "cpu")
+processor.to("cuda" if torch.cuda.is_available() else "cpu")
 
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
