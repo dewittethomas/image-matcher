c7a6d7277fc1316be22e27aa5c7c859151a80618
Thomas Dewitte <thomasdewittecontact@gmail.com>
2025-09-10 06:39:18 +0200

    Added CUDA usage + print similarity score

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from transformers import CLIPModel, CLIPProcessor
 model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 
+model.to("cuda" if torch.cuda.is_available() else "cpu")
+
 url = "http://images.cocodataset.org/val2017/000000039769.jpg"
 image = Image.open(requests.get(url, stream=True).raw)
 
@@ -14,3 +16,4 @@ inputs = processor(text=["a photo of a cat", "a photo of a dog"], images=image, 
 
 outputs = model(**inputs)
 logits_per_image = outputs.logits_per_image
+print(logits_per_image)
